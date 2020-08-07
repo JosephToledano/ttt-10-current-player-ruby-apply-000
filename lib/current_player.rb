ba8a6f6185b38a794d37ff24d6c314b2729ad126
Joseph Toledano <joseph.a.toledano@gmail.com>
2020-08-07 02:01:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,4 +2,13 @@ def turn_count(board)
   moves = 0
   board.each do |turn|
     moves += 1
+  return moves 
+end 
+
+def current_player(board)
+  if moves % 2 == 0
+    return 'X'
+  else 
+    return 'O'
+  end
 end 
